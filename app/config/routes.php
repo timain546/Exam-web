d3743f8ac88697062da3d259675189f5b621e9ee
Ndroso <ndrosotianarivo@gmail.com>
2026-02-16 15:11:09 +0300

    routes dons
    2 lignes 19 , 20

--- app/config/routes.php
+++ app/config/routes.php
@@ -16,4 +16,8 @@ $router->group('', function(Router $router) use ($app) {
     $router->get('/', [ TemplateController::class, 'home' ]);
     $router->get('/products', [ TemplateController::class, 'products' ]);
 
+    $router->get('/dons/create', [DonsController::class, 'showFormDons']);
+    $router->post('/api/dons/create', [DonsController::class, 'createDons']);
+
+
 }, [ SecurityHeadersMiddleware::class ]);
