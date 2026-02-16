ae3141e131a3ed7cc4eb712bf54000a7cc3ad82c
Ndroso <ndrosotianarivo@gmail.com>
2026-02-16 16:33:20 +0300

    function besoin
    getTotalBesoinsParProduit

--- app/models/Besoins.php
+++ app/models/Besoins.php
@@ -10,4 +10,10 @@ class Besoins {
         $stt = $this->pdo->prepare('INSERT INTO bngrc_besoins(date_besoin, id_ville, id_produit, quantite) VALUES (?, ?, ?, ?)');
         $stt->execute([$input['date_besoin'], $input['id_ville'], $input['id_produit'], $input['quantite']]);
     }
+
+    public function getTotalBesoinsParProduit($id_ville){
+        $stmt = $this->pdo->prepare("SELECT id_produit, SUM(quantite) as quantite FROM bngrc_besoins WHERE id_ville = ? GROUP BY id_produit");
+        $stmt->execute([$id_ville]);
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
 }
