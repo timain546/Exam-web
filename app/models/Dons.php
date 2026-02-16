72b6c88df3babf27acda43037a97a67002b15ffe
Ndroso <ndrosotianarivo@gmail.com>
2026-02-16 16:34:13 +0300

    function Dons
    getTotalDonsParProduit

--- app/models/Dons.php
+++ app/models/Dons.php
@@ -14,4 +14,10 @@ class Dons{
       $stt->execute([$input['date_don'], $input['id_produit'], $input['quantite']]);
   }
 
+  public function getTotalDonsParProduit(){
+        $stmt = $this->pdo->prepare("SELECT id_produit, SUM(quantite) as quantite FROM bngrc_dons GROUP BY id_produit");
+        $stmt->execute();
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
 }
