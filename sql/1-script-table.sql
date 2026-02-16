e9f9fd081438d7a1d4d30433bb7e5685422cb7f6
Ndroso <ndrosotianarivo@gmail.com>
2026-02-16 17:08:54 +0300

    dispatch
    table

--- sql/1-script-table.sql
+++ sql/1-script-table.sql
@@ -44,3 +44,12 @@ CREATE TABLE IF NOT EXISTS bngrc_dons(
     quantite INT NOT NULL,
     FOREIGN KEY (id_produit) REFERENCES bngrc_produits(id_produit)
 );
+
+CREATE TABLE IF NOT EXISTS bngrc_dispatch(
+    id_dispatch INT AUTO_INCREMENT PRIMARY KEY,
+    id_don INT NOT NULL,
+    quantite INT NOT NULL,
+    id_ville INT NOT NULL,
+    FOREIGN KEY (id_don) REFERENCES bngrc_dons(id_don),
+    FOREIGN KEY (id_ville) REFERENCES bngrc_villes(id_ville)
+);
