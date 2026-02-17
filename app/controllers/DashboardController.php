1aa02243c8d5a119d57e6c651e6ea444f4561a1e
timain546 <timainrakotondranjato@gmail.com>
2026-02-17 08:39:43 +0300

    ajout du fichier DashboardController.php et modification de Ville.php

---
+++ app/controllers/DashboardController.php
@@ -0,0 +1,28 @@
+<?php
+namespace app\controllers;
+
+use app\models\Ville;
+use flight\Engine;
+
+class DashboardController {
+    private $app;
+    private $ville;
+
+    public function __construct(Engine $app) {
+        $this->app = $app;
+        $this->ville = new Ville($this->app->db());
+    }
+
+    public function dashboard() {
+        $villes = $this->ville->getAllVilles();
+        $result = [];
+        foreach($ville as $v) {
+            $result[] = [
+                'id_ville' => $v['id_ville'],
+                'nom' => $v['nom'],
+                'produits' => $this->ville->getProduitsDeVille($v['id_ville'])
+            ];
+        }
+        $this->app->render('dashboard', ['villes' => $result]);
+    }
+}
--- app/models/Ville.php
+++ app/models/Ville.php
@@ -14,4 +14,9 @@ class Ville{
     return $stmt->fetchAll();
   }
 
+  public function getProduitsDeVille($id_ville) {
+    $stmt = $this->pdo->prepare('SELECT p.id_produit as id_produit, p.nom as nom, b.quantite as besoin, b.quantite - b.quantite_restante as don FROM bngrc_v_besoins_totaux b JOIN bngrc_produits ON b.id_produit = p.id_produit WHERE b.id_ville = ?');
+    $stmt->execute([$id_ville]);
+    return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+  }
 }
