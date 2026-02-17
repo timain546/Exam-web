db0ededfda3161272aaf85c9f5885cb7a6df8959
r-tokiniaina <r.tokiniaina@outlook.com>
2026-02-17 16:12:19 +0300

    reinit

--- app/utils/Utils.php
+++ app/utils/Utils.php
@@ -7,9 +7,10 @@ class Utils {
     public static function reinitAll(\PDO $pdo) {
       $pdo->exec("DELETE FROM bngrc_besoins");
       $pdo->exec("DELETE FROM bngrc_dons");
+      $pdo->exec("DELETE FROM bngrc_achat");
       $pdo->exec("INSERT INTO bngrc_besoins (date_besoin, id_ville, id_produit, quantite, quantite_restante) VALUES
-          ('2026-02-10', 2, 1, 120, 120),  -- Mahanoro
-          ('2026-02-10', 2, 3, 85, 85),   -- Mahanoro
+          ('2026-02-10', 2, 1, 120, 120),
+          ('2026-02-10', 2, 3, 85, 85),
           ('2026-02-11', 5, 4, 30, 30),   -- Foulpointe
           ('2026-02-12', 3, 2, 70, 70),   -- Vatomandry
           ('2026-02-13', 1, 5, 200, 200),  -- Toamasina
