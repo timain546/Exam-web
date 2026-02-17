8bdcc8b1eb2b5effb1deaa51fc43d609dd305606
r-tokiniaina <r.tokiniaina@outlook.com>
2026-02-17 10:42:06 +0300

    Revert "ok"
    This reverts commit 38490796a14580d3e4a18928f104c5245054480f.

--- app/controllers/DashboardController.php
+++ app/controllers/DashboardController.php
@@ -2,6 +2,7 @@
 namespace app\controllers;
 
 use app\models\Ville;
+use app\utils\Utils;
 use flight\Engine;
 
 class DashboardController {
@@ -26,10 +27,12 @@ class DashboardController {
         $this->app->render('dashboard', ['villes' => $result]);
     }
 
-    public function reinit(){
-      $stmt = $this->pdo->prepare("DELETE * FROM bngrc_besoins");
-      $stmt->execute();
-      return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    public function showReinit() {
+        $this->app->render('reinit');
     }
-  }
+
+    public function reinit() {
+        Utils::reinitAll($this->app->db());
+        $this->app->json(['status' => 'ok']);
+    }
 }
